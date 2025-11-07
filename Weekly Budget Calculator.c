3c84fde4be52a3e55edd05e0b8de91310207f0fb
wiston1568 <wistonanold2@gmail.com>
2025-11-07 17:12:52 +0300

    starting work

--- Weekly Budget Calculator.c
+++ Weekly Budget Calculator.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
+int main(){
+    float budget,expenses[7],total spent=0,remaining;
+    int;
+    //input section
+    printf("enter ur weekly budget:");
+    scanf("%f",budget);
 
+    printf("\n enter ur daily expenses for 7 days\n");
+    for(i=0;i<7;i++){
+printf("day %d:",i+1);
+scanf("%f",&expenses[i]);
+total spent += expenses[i];
+    }
+}
